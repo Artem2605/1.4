f60c8d59c6b4da063bcba24364b1c3e597a65d7f
Artem2605 <artyom260599@mail.ru>
2023-11-04 21:30:03 +0300

    forKata

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,31 +1,23 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-import java.util.*;
+import java.util.List;
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class Main {
+
     public static void main(String[] args) {
-        Util.getConnection();
-
+        getConnection();
         UserServiceImpl user = new UserServiceImpl();
-
         user.createUsersTable();
-
         user.saveUser("Alex", "Ivanov", (byte) 26);
         user.saveUser("Masha", "Petrova", (byte) 20);
         user.saveUser("Vasya", "Romanov", (byte) 22);
         user.saveUser("Lena", "Leonova", (byte) 24);
-
         user.removeUserById(2);
-
         List<User> list = user.getAllUsers();
-
         user.cleanUsersTable();
-
         user.dropUsersTable();
     }
 }
